b141d42ce4a7d675afb75401b356645ee4b50629
Mr Muhammad Usman <82950524+Muhammad-Usman-Laravel-Developer@users.noreply.github.com>
2022-12-29 16:28:25 +0500

    uncomment team controller code
    uncomment team controller code

--- app/Http/Controllers/backend/TeamController.php
+++ app/Http/Controllers/backend/TeamController.php
@@ -23,30 +23,30 @@ class TeamController extends Controller
         return view('backend.team.add',compact('services'));
     }
 
-    // public function save(Request $request)
-    // {
-    //     $request->validate([
-    //         'name'=>'required',
-    //         'designation'=>'required',
-    //         'image'=>'required',
-    //     ]);
+    public function save(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'designation'=>'required',
+            'image'=>'required',
+        ]);
 
-    //     $add=new Team();
-    //     $add->name=$request->name;
-    //     $add->designation=$request->designation;
-    //     $add->facebook_url=$request->facebook_url;
-    //     $add->instagram_url=$request->instagram_url;
-    //     $add->twitter_url=$request->twitter_url;
-    //     $add->linkedin_url=$request->linkedin_url;
-    //     if($request->file('image'))
-    //     {
-    //         $filename = time().rand(2,9999).'.'.$request->file('image')->extension();
-    //         $move = $request->file('image')->move(public_path().'/assets/frontend/images/',$filename);
-    //         $add->image = $filename;
-    //     }
-    //     $add->save();
-    //     return redirect()->route('admin_team');
-    // }
+        $add=new Team();
+        $add->name=$request->name;
+        $add->designation=$request->designation;
+        $add->facebook_url=$request->facebook_url;
+        $add->instagram_url=$request->instagram_url;
+        $add->twitter_url=$request->twitter_url;
+        $add->linkedin_url=$request->linkedin_url;
+        if($request->file('image'))
+        {
+            $filename = time().rand(2,9999).'.'.$request->file('image')->extension();
+            $move = $request->file('image')->move(public_path().'/assets/frontend/images/',$filename);
+            $add->image = $filename;
+        }
+        $add->save();
+        return redirect()->route('admin_team');
+    }
 
     public function edit($id)
     {
