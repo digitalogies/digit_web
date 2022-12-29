2cc83fb7700de8eed3753e3bca7803ab9febb629
Mr Muhammad Usman <82950524+Muhammad-Usman-Laravel-Developer@users.noreply.github.com>
2022-12-29 19:29:16 +0500

    teamController comment
    teamController comment

--- app/Http/Controllers/backend/TeamController.php
+++ app/Http/Controllers/backend/TeamController.php
@@ -13,65 +13,63 @@ class TeamController extends Controller
 {
     public function view()
     {
-        $allrecord=Team::get();
-        return view('backend.team.view',compact('allrecord'));
+        $allrecord = Team::get();
+        return view('backend.team.view', compact('allrecord'));
     }
 
     public function add()
     {
-        $services=Services::get();
-        return view('backend.team.add',compact('services'));
+        $services = Services::get();
+        return view('backend.team.add', compact('services'));
     }
 
     public function save(Request $request)
     {
         $request->validate([
-            'name'=>'required',
-            'designation'=>'required',
-            'image'=>'required',
+            'name' => 'required',
+            'designation' => 'required',
+            'image' => 'required',
         ]);
 
-        $add=new Team();
-        $add->name=$request->name;
-        $add->designation=$request->designation;
-        $add->facebook_url=$request->facebook_url;
-        $add->instagram_url=$request->instagram_url;
-        $add->twitter_url=$request->twitter_url;
-        $add->linkedin_url=$request->linkedin_url;
-        if($request->file('image'))
-        {
-            $filename = time().rand(2,9999).'.'.$request->file('image')->extension();
-            $move = $request->file('image')->move(public_path().'/assets/frontend/images/',$filename);
+        $add = new Team();
+        $add->name = $request->name;
+        $add->designation = $request->designation;
+        $add->facebook_url = $request->facebook_url;
+        $add->instagram_url = $request->instagram_url;
+        $add->twitter_url = $request->twitter_url;
+        $add->linkedin_url = $request->linkedin_url;
+        if ($request->file('image')) {
+            $filename = time() . rand(2, 9999) . '.' . $request->file('image')->extension();
+            $move = $request->file('image')->move(public_path() . '/assets/frontend/images/', $filename);
             $add->image = $filename;
         }
         $add->save();
         return redirect()->route('admin_team');
     }
 
-    public function edit($id)
-    {
-        $edit=Team::find($id);
-        return view('backend.team.edit',compact('edit'));
-    }
+    // public function edit($id)
+    // {
+    //     $edit=Team::find($id);
+    //     return view('backend.team.edit',compact('edit'));
+    // }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
         $request->validate([
-            'name'=>'required',
-            'designation'=>'required',
+            'name' => 'required',
+            'designation' => 'required',
         ]);
 
-        $update=Team::find($id);
-        $update->name=$request->name;
-        $update->designation=$request->designation;
-        $update->facebook_url=$request->facebook_url;
-        $update->instagram_url=$request->instagram_url;
-        $update->twitter_url=$request->twitter_url;
-        $update->linkedin_url=$request->linkedin_url;
-        if($request->file('image'))
-        {
-            $filename = time().rand(2,9999).'.'.$request->file('image')->extension();
-            $move = $request->file('image')->move(public_path().'/assets/frontend/images/',$filename);
+        $update = Team::find($id);
+        $update->name = $request->name;
+        $update->designation = $request->designation;
+        $update->facebook_url = $request->facebook_url;
+        $update->instagram_url = $request->instagram_url;
+        $update->twitter_url = $request->twitter_url;
+        $update->linkedin_url = $request->linkedin_url;
+        if ($request->file('image')) {
+            $filename = time() . rand(2, 9999) . '.' . $request->file('image')->extension();
+            $move = $request->file('image')->move(public_path() . '/assets/frontend/images/', $filename);
             $update->image = $filename;
         }
         $update->update();
@@ -80,10 +78,9 @@ class TeamController extends Controller
 
     public function delete($id)
     {
-        $delete=Team::find($id);
-        $image_path = "public/assets/frontend/images/".$delete->image;
-        if(File::exists($image_path))
-        {
+        $delete = Team::find($id);
+        $image_path = "public/assets/frontend/images/" . $delete->image;
+        if (File::exists($image_path)) {
             File::delete($image_path);
         }
         $delete->delete();
